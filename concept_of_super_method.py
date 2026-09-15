d4b7828c08601358a9ff9f42e857940d1044bd27
deshanshakya <deshu.shakya.7@gmail.com>
2026-09-15 15:34:56 +0545

    Add files via upload

---
+++ concept_of_super_method.py
@@ -0,0 +1,32 @@
+# super() method calls the parent class method not child class method
+class Polygon:
+    def __init__(self, sides):
+        self.sides = sides
+
+    def display_info(self):
+        print("A polygon is a two dimensional shape with straight lines.")
+
+    def get_perimeter(self):
+        perimeter = sum(self.sides)
+        return perimeter
+
+class Triangle(Polygon):
+    def display_info(self):
+
+
+        print("A triangle is a polygon with 3 edges.")
+
+
+        # call the display_info() method of Polygon (which is the parent class)
+        super().display_info()
+
+
+# create an object of Triangle
+t1 = Triangle([5, 6, 7])
+
+# call get_perimeter using t1
+perimeter = t1.get_perimeter()
+print("Perimeter:", perimeter)
+
+# call display_info() using t1
+t1.display_info()
